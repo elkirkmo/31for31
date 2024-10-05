9d96c9ba46e2b4b177a3bb2b345e3ac98c2303e6
Chris Kirkham <me@chriskirkham.com>
2024-10-04 21:50:48 -0600

    added components for a footer and a sweet-ass smoke effect. Updates to listing and layout

---
+++ src/components/footer.svelte
@@ -0,0 +1,21 @@
+<footer class="mb-7">
+  <h5 class="mb-5">
+    Made with love by <a
+      href="https://github.com/elkirkmo/31for31"
+      target="_blank"
+      class="text-green">@elkirkmo</a
+    >. Hail yourself.
+  </h5>
+  <i class="text-sm"
+    >This website is not owned, operated, or even acknowledge by LPN or any of
+    its affillates. Links are valid only in the US region of various streaming
+    sites. This website does not aim to make money through affiliate links or
+    sign-up referrals or any such shenannigans and is only for informational
+    purposes. If you like what I did here and wanna help out or buy me a beer,
+    hit me up through <a
+      href="https://github.com/elkirkmo/31for31"
+      target="_blank"
+      class="text-green">github</a
+    >.
+  </i>
+</footer>
