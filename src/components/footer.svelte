295eaa8a93b506c9bfef62b0a41bb9bb32b14336
Chris Kirkham <chris@Chriss-MacBook-Pro-2.local>
2026-05-28 17:02:57 -0600

    Fixing some issues with syntax and spelling. Added svelte:head

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -10,7 +10,7 @@
     >This website is not owned, operated, or even acknowledge by LPN or any of
     its affillates. Links are valid only in the US region of various streaming
     sites. This website does not aim to make money through affiliate links or
-    sign-up referrals or any such shenannigans and is only for informational
+    sign-up referrals or any such shenanigans and is only for informational
     purposes. If you like what I did here and wanna help out or buy me a beer,
     hit me up through <a
       href="https://github.com/elkirkmo/31for31"
